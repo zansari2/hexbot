17d71686a75804c11651ddaf1e46d68eefec7ec3
Zishan Ansari <42755217+zansari2@users.noreply.github.com>
2019-07-15 12:24:26 -0400

    Changes to base project for experimentation

--- app/scripts.js
+++ app/scripts.js
@@ -22,12 +22,16 @@ request.onload = function() {
       const h1 = document.createElement('h1')
       h1.textContent = movie.title
 
+      const h2 = document.createElement('h2')
+      h2.textContent = movie.release_date
+
       const p = document.createElement('p')
       movie.description = movie.description.substring(0, 300)
       p.textContent = `${movie.description}...`
 
       container.appendChild(card)
       card.appendChild(h1)
+      card.appendChild(h2)
       card.appendChild(p)
     })
   } else {
